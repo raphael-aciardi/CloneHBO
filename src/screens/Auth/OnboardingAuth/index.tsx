2d10f8896895ddb59dbaf3aa7164f14c0fe18664
Raphael Aciardi <996193248ph@gmail.com>
2025-11-30 21:17:49 -0300

    choice: Configurando styled components

--- src/screens/Auth/OnboardingAuth/index.tsx
+++ src/screens/Auth/OnboardingAuth/index.tsx
@@ -1,10 +1,11 @@
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Title } from './styles';
 
 export default function OnboardingAuth() {
   return (
     <SafeAreaView>
-      <Text>OnboardingAuth</Text>
+      <Title>OnboardingAuth</Title>
     </SafeAreaView>
   );
 }
